13f932565c2281416ea3048b23d4d27b8f58b4ec
Desalegn Berihun <desunew@gmail.com>
2019-01-19 04:12:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,19 @@
 function basicTeenager(age) {
-
+  if (age >13 && age <19)
+  return "You are a teebager!"
 }
 
 function teenager(age) {
-
+  if (age >13 && age <19)
+  "You are a teebager!"
+}else {
+  return "You are not a teenager!"
 }
+
 
 function ageChecker(age) {
 
-}
-
+  }
 function ternaryTeenager(age) {
 
 }
